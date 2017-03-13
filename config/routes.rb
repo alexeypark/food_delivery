6ade218f4880535974403abd89738361a5f1d416
Alexey Park <alexey.g.park@gmail.com>
2017-03-13 19:57:19 +0600

    Add gems: active_record, devise, cancancan, simple_form.
    Add user devise, g active_admin: User, add role to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords'}
+
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"
+    authenticated :user do
+      root :to => 'admin/dashboard#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'users/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  ActiveAdmin.routes(self)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
